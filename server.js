0bc49af81f35a989edd3426b2f6d46d7940ef5c3
Spiralistsameer <sameer@modularitylabs.com>
2025-04-09 19:30:23 +0530

    port changed

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const cors = require('cors');
 
 
 //middle ware
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 10000
 const app = express()
 app.use(cors());
 app.use(express.json())
